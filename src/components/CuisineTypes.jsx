c8d44effe44bff55c910f55fd32d0e50ba2cca02
Pil Kyu Cho <comsci.p.cho@gmail.com>
2024-04-21 23:22:14 +0200

    add search filter for cuisine type

--- src/components/CuisineTypes.jsx
+++ src/components/CuisineTypes.jsx
@@ -14,35 +14,35 @@ const CuisineTypes = () => {
                 Burgers
             </div>
             <div className='cuisine-type-item'>
-                <div className='cuisine-type-icon kebab-icon'/>
+                <div className='cuisine-type-icon kebab-icon' onClick={() => filterForCuisine("Kebab")} />
                 Kebab
             </div>
             <div className='cuisine-type-item'>
-                <div className='cuisine-type-icon sushi-icon'/>
+                <div className='cuisine-type-icon sushi-icon' onClick={() => filterForCuisine("Sushi")} />
                 Sushi
             </div>
             <div className='cuisine-type-item'>
-                <div className='cuisine-type-icon pizza-icon'/>
+                <div className='cuisine-type-icon pizza-icon' onClick={() => filterForCuisine("Pizza")} />
                 Pizza
             </div>
             <div className='cuisine-type-item'>
-                <div className='cuisine-type-icon chinese-icon'/>
+                <div className='cuisine-type-icon chinese-icon' onClick={() => filterForCuisine("Chinese")} />
                 Chinese
             </div>
             <div className='cuisine-type-item'>
-                <div className='cuisine-type-icon greek-icon'/>
+                <div className='cuisine-type-icon greek-icon' onClick={() => filterForCuisine("Greek")} />
                 Greek
             </div>
             <div className='cuisine-type-item'>
-                <div className='cuisine-type-icon grocery-icon'/>
+                <div className='cuisine-type-icon grocery-icon' onClick={() => filterForCuisine("Groceries")} />
                 Groceries
             </div>
             <div className='cuisine-type-item'>
-                <div className='cuisine-type-icon chicken-icon'/>
+                <div className='cuisine-type-icon chicken-icon' onClick={() => filterForCuisine("Chicken")} />
                 Chicken
             </div>
             <div className='cuisine-type-item'>
-                <div className='cuisine-type-icon indian-icon'/>
+                <div className='cuisine-type-icon indian-icon' onClick={() => filterForCuisine("Indian")} />
                 Indian
             </div>
         </div>
