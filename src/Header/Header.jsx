74f81eed70921d23bb6705d111295475cf8ef79a
Pil Kyu Cho <comsci.p.cho@gmail.com>
2024-04-21 11:46:59 +0200

    define Home page header

---
+++ src/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchBar from "../components/SearchBar";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='holder'>
+        <header className='header'>
+            <div className='header-content flex flex-c text-center text-white'>
+                <h2 className='header-title text-capitalize'>Just Eat Takeaway</h2><br />
+                {/*<SearchBar />*/}
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default Header
