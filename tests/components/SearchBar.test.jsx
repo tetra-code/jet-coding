9640195f7bcd481f34f73b17766b73672a684ad9
Pil Kyu Cho <comsci.p.cho@gmail.com>
2024-04-22 10:56:53 +0200

    testing setup

---
+++ tests/components/SearchBar.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {fireEvent, render, screen} from '@testing-library/react';
+import SearchBar from '../../src/components/SearchBar';
+
+
+test('submits search query on Enter key press', () => {
+});
