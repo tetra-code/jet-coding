32a8d99e2d74d82c04616db2347ef2b60b6f25d7
Pil Kyu Cho <comsci.p.cho@gmail.com>
2024-04-22 14:09:49 +0200

    fix searchbar test

--- tests/components/SearchBar.test.jsx
+++ tests/components/SearchBar.test.jsx
@@ -17,47 +17,30 @@ jest.mock('../../src/utils/context', () => ({
 }));
 
 describe('SearchBar', () => {
-    let setSearchTerm, setResultTitle, setCuisineType, handleSearch, handleSubmit;
+    const searchBarPlaceholderText = "Type a UK postcode";
+    let setSearchTerm, setResultTitle, setCuisineType;
     const validSearchTerm1 = "EC4M7RF";
-    const validSearchTerm2 = "EC4M 7RF";
-    const validSearchTerm3 = "EC 4M 7RF";
-    const validSearchTerm4 = "CT1 2EH";
-    const validSearchTerm5 = "BS1 4DJ";
-    const validSearchTerm6 = "PL4 0DW";
-    const validSearchTerm7 = "G3 8AG";
-    const validSearchTerm8 = "BN1 1AE";
-    const validSearchTerm9 = "M16 ORA";
-    const validSearchTerm10 = "L4 0TH";
-    const validSearchTerm11 = "EC4M";
+    const validSearchTerm2 = "ec4m7rf";
+    const validSearchTerm3 = "G3 8AG";
+    const validSearchTerm4 = "BN1 1AE";
+
     const invalidSearchTerm1 = "SE11";
-    const invalidSearchTerm2 = "SE 11";
-    const invalidSearchTerm3 = "EC4M7Rf";
-    const invalidSearchTerm4 = "EC4M7R";
-    const invalidSearchTerm5 = "";
-    const invalidSearchTerm6 = "E";
-    const invalidSearchTerm7 = "%";
-    const invalidSearchTerm8 = "EC4M7R#";
-    const invalidSearchTerm9 = "ec4m7rf";
-    const invalidSearchTerm10 = "EC4M7RF3";
-    const invalidSearchTerm11 = "fds3f4edf";
-    const invalidSearchTerm12 = "151DEN U8";
-    const invalidSearchTerm13 = "@#f8dNU8";
-    const invalidSearchTerm14 = "1df83did djf83j932nfdiow8ff3f";
+    const invalidSearchTerm2 = "E";
+    const invalidSearchTerm3 = " ";
+    const invalidSearchTerm4 = "\n";
+    const emptySearchTerm = "";
+
 
     beforeEach(() => {
         setSearchTerm = jest.fn();
         setResultTitle = jest.fn();
         setCuisineType = jest.fn();
-        handleSearch = jest.fn();
-        handleSubmit = jest.fn();
 
         useGlobalContext.mockImplementation(() => ({
             setSearchTerm,
             setResultTitle,
             cuisineType: '',
             setCuisineType,
-            handleSearch,
-            handleSubmit
         }));
     });
 
@@ -70,17 +53,22 @@ describe('SearchBar', () => {
         expect(screen.getByPlaceholderText(searchBarPlaceholderText)).toBeInTheDocument();
     });
 
-    it('updates search term when valid search term is entered', () => {
+    it('updates searchTerm when valid search term is entered but not resultTitle', () => {
+        expect(setSearchTerm).not.toHaveBeenCalled();
+        expect(setResultTitle).not.toHaveBeenCalled();
+
         render(
             <MemoryRouter>
                 <SearchBar />
             </MemoryRouter>
         );
         const input = screen.getByPlaceholderText(searchBarPlaceholderText);
-        fireEvent.change(input, { target: { value: validSearchTerm2 } });
+        fireEvent.change(input, { target: { value: validSearchTerm1 } });
         const searchIcon = screen.getByTestId('search-icon');
         fireEvent.click(searchIcon);
-        // expect(handleSubmit).toHaveBeenCalled();
-        expect(setSearchTerm).toBe()
+
+        expect(setSearchTerm).toHaveBeenCalled();
+        expect(setSearchTerm.mock.calls[0][0]).toBe(validSearchTerm1)
+        expect(setResultTitle).not.toHaveBeenCalled();
     });
 });
