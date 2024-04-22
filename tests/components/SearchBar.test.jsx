b268da8414bfe8b09320d4d6f1787072ae944d9f
Pil Kyu Cho <comsci.p.cho@gmail.com>
2024-04-22 12:03:39 +0200

    fix jest testing config to run tests

--- tests/components/SearchBar.test.jsx
+++ tests/components/SearchBar.test.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
-import {fireEvent, render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { useGlobalContext } from '../../src/utils/context';
 import SearchBar from '../../src/components/SearchBar';
+import '@testing-library/jest-dom';
 
+jest.mock('../../src/utils/context', () => ({
+    useGlobalContext: jest.fn(),
+}));
 
-test('submits search query on Enter key press', () => {
+describe('SearchBar', () => {
+    let setSearchTerm, setResultTitle, setCuisineType;
+
+    beforeEach(() => {
+        setSearchTerm = jest.fn();
+        setResultTitle = jest.fn();
+        setCuisineType = jest.fn();
+
+        useGlobalContext.mockImplementation(() => ({
+            setSearchTerm,
+            setResultTitle,
+            cuisineType: '',
+            setCuisineType,
+        }));
+    });
+
+    it('renders without crashing', () => {
+        render(
+            <MemoryRouter>
+                <SearchBar />
+            </MemoryRouter>
+        );
+        expect(screen.getByPlaceholderText("Type a UK postcode")).toBeInTheDocument();
+    });
 });
