39876f01a06966bdec2dc963ccd4e0f91ec9b08b
Pil Kyu Cho <comsci.p.cho@gmail.com>
2024-04-23 22:29:36 +0200

    create Header component test

---
+++ frontend/tests/header/Header.test.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header from '../../src/header/Header';
+import '@testing-library/jest-dom';
+import {MemoryRouter} from "react-router-dom";
+
+jest.mock('../../src/utils/context', () => ({
+    useGlobalContext: jest.fn().mockReturnValue({
+        setSearchTerm: jest.fn(),
+        setResultTitle: jest.fn(),
+        cuisineType: '',
+        setCuisineType: jest.fn(),
+        isDeliveryMode: true,
+        setDeliveryMode: jest.fn()
+    }),
+}));
+
+
+describe('Header', () => {
+    it('renders without crashing and the correct title', () => {
+        render(
+            <MemoryRouter>
+                <Header />
+            </MemoryRouter>
+        );
+        const title = screen.getByRole('heading');
+        expect(title).toHaveTextContent('Just Eat Takeaway');
+    });
+
+    // Test 2: Header renders SearchBar and Toggle components
+    it('renders SearchBar and Toggle components', () => {
+        render(
+            <MemoryRouter>
+                <Header />
+            </MemoryRouter>
+        );
+        const searchBar = screen.getByRole('textbox');
+        const toggle = screen.getByRole('checkbox');
+        expect(searchBar).toBeInTheDocument();
+        expect(toggle).toBeInTheDocument();
+    });
+});
